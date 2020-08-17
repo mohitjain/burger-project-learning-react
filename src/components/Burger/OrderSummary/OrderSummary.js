9284b84603644823497d0800ab6a3e01dd93fbc4
Mohit Jain <jain.mohit27@gmail.com>
2020-08-18 04:47:11 +0530

    Order Cancel and Order Continue dummy buttons

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients).map(ingredient => {
@@ -15,6 +16,8 @@ const orderSummary = (props) => {
                     {ingredientSummary}
                 </ul>
                 <p>Continue to Checkout</p>
+                <Button clicked={props.purchaseCancelled} btnType='Danger'>CANCEL</Button>
+                <Button clicked={props.purchaseContinued} btnType='Success'>CONTINUE</Button>
             </Aux>
         </div>
     );
