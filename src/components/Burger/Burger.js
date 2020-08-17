5821d1405e8353ccc8b0105d35bf2a701653e17e
Mohit Jain <jain.mohit27@gmail.com>
2020-08-17 07:39:53 +0530

    Dummy Add more or less buttons with basic display

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,19 @@ import classes from './Burger.module.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients).map(ingredient => {
+    let transformedIngredients = Object.keys(props.ingredients).map(ingredient => {
         return [...Array(props.ingredients[ingredient])].map((_, index) => {
-            return <BurgerIngredient key={ingredient+1} type={ingredient}/>;
+            return <BurgerIngredient key={ingredient + index } type={ingredient}/>;
         });
-    });
+    }).reduce((old_array, element) => {
+        return old_array.concat(element);
+    }, []);
+
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding some ingredients.</p>
+    }
+
+    console.log(transformedIngredients);
 
     return (
         <div className={classes.Burger}>
